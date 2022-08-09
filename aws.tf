219059f13a62a47955365d3ea492a47cab99767a
SumalathaMangala <110534957+SumalathaMangala@users.noreply.github.com>
2022-08-09 10:32:57 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "sumalatha" {
   ami = "ami-090fa75af13c156b4"
   instance_type = "t2.micro"
-  count = 3
-  key_name = "Terraform" 
+  count = 1
+  subnet_id = "subnet-0c34204c7c67d2684"
 
   tags = {
     Name = "sumalatha"
-        team = "dev"
+        
 
 
   }
